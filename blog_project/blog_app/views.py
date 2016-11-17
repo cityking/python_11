16b178993596688b69cc59efb954558f25af5d06
root <root@localhost.localdomain>
2016-11-17 23:52:30 +0800

    文章分页显示

--- blog_project/blog_app/views.py
+++ blog_project/blog_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 import logging
 from django.conf import settings
-from models import Category
+from models import Category, Article
+from django.core.paginator import Paginator,InvalidPage,EmptyPage,PageNotAnInteger
 
 logger = logging.getLogger('logger_test')
 
@@ -15,6 +16,25 @@ def global_setting(request):
 
 # Create your views here.
 def index(request):
+	page_data = 2
 	category_list = Category.objects.all()	
 	logger.debug('this is a debug')
-	return render(request, 'index.html', {"category_list" : category_list})
+
+	article_list = Article.objects.all()
+	try:
+		page = int(request.GET.get('page','1'))
+		page_all = int(request.GET.get('page_all', '1'))
+	except ValueError:
+		page = 1
+		page_all =1
+	if page == 1 and page_all == 1:
+		page_all = Article.objects.count()/page_data
+		if Article.objects.count()%page_data > 0:
+			page_all = page_all+1
+		
+	paginator = Paginator(article_list, page_data)
+	try:
+		article_list = paginator.page(page)
+	except(InvalidPage,EmptyPage,PageNotAnInteger):
+		article_list = paginator.page(1)
+	return render(request, 'index.html', locals())
