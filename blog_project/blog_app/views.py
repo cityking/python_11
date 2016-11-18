e8ceed0e60ec26e69e375715e29f5ae458696e2f
root <root@localhost.localdomain>
2016-11-18 19:04:09 +0800

    自己实现分页

--- blog_project/blog_app/views.py
+++ blog_project/blog_app/views.py
@@ -1,3 +1,4 @@
+# encoding: utf-8
 from django.shortcuts import render
 import logging
 from django.conf import settings
@@ -18,12 +19,16 @@ def global_setting(request):
 def index(request):
 	page_data = 2
 	category_list = Category.objects.all()	
-	logger.debug('this is a debug')
 
-	article_list = Article.objects.all()
+	#日志器的使用
+	#logger.debug('this is a debug')
+
+	#文章显示
+	#article_list = Article.objects.all()
 	try:
 		page = int(request.GET.get('page','1'))
 		page_all = int(request.GET.get('page_all', '1'))
+		page_type = request.GET.get('page_type')
 	except ValueError:
 		page = 1
 		page_all =1
@@ -31,10 +36,20 @@ def index(request):
 		page_all = Article.objects.count()/page_data
 		if Article.objects.count()%page_data > 0:
 			page_all = page_all+1
-		
-	paginator = Paginator(article_list, page_data)
-	try:
-		article_list = paginator.page(page)
-	except(InvalidPage,EmptyPage,PageNotAnInteger):
-		article_list = paginator.page(1)
+	if page_type=='up':
+		page = page-1
+	if page_type=='down':
+		page = page+1
+	page_start = (page-1)*page_data
+	if page == page_all:
+		page_end = Article.objects.count()
+	else:
+		page_end = page*page_data
+
+	article_list = Article.objects.all()[page_start:page_end]	
+#	paginator = Paginator(article_list, page_data)
+#	try:
+#		article_list = paginator.page(page)
+#	except(InvalidPage,EmptyPage,PageNotAnInteger):
+#		article_list = paginator.page(1)
 	return render(request, 'index.html', locals())
